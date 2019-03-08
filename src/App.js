d87a942a67a9d8632a611772f9a1026d0922b3b4
Daniel Chernyshev <droque123@gmail.com>
2019-03-08 13:49:44 +0300

    Ready multiple selector, For syles use antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import MultipleSelector from "./components/MultipleSelector";
+
+
+const elements = [
+    {
+        key: "1",
+        value: "Item 1"
+    },
+    {
+        key: "2",
+        value: "Item 2"
+    },
+    {
+        key: "3",
+        value: "Item 3"
+    },
+    {
+        key: "4",
+        value: "Item 4"
+    },
+    {
+        key: "5",
+        value: "Item 5"
+    }
+];
 
 class App extends Component {
+
+    onChangeMultipleSelector(data) {
+        console.log(data)
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <div style={{width: 400, height: 400, padding: 5, backgroundColor: 'white'}}>
+                <MultipleSelector
+                    data={elements}
+                    selected={["1", "2"]}
+                    onChange={this.onChangeMultipleSelector}
+                    selectText={'Add Item'}
+                    deleteText={'Delete Item'}
+                    size='default'
+                />
+            </div>
         </header>
       </div>
     );
---
+++ src/components/MultipleSelector.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Select } from 'antd';
+
+const Option = Select.Option;
+
+class MultipleSelector extends Component {
+
+    defaultSelectText = "";
+
+    state = {
+        selected: [],
+        newSelector: ""
+    };
+
+    componentWillMount() {
+        this.setState({
+            selected: this.props.selected,
+            newSelector: this.props.selectText
+        });
+
+        this.defaultSelectText = this.props.selectText;
+    }
+
+    async handleChange(item, index) {
+        if (item.key === "delete") {
+            await this.state.selected.splice(index, 1);
+        } else if (index === "new") {
+            this.state.selected.push(item.key);
+            this.state.newSelector = this.defaultSelectText;
+        } else {
+            this.state.selected[index] = item.key;
+        }
+        await this.forceUpdate();
+
+        this.props.onChange(this.state.selected);
+    }
+
+    render() {
+        const {data, deleteText, size} = this.props;
+
+        return (
+            <div className="multiple-selector-container">
+                {
+                    this.state.selected.map((item, index) => (
+                        <div key={`selector-${index}`} >
+                            <Select labelInValue value={{ key: (data.find(d => d.key === item)).value}} style={{width: '100%'}} size={size}
+                                    onChange={item => this.handleChange(item, index)}>
+                                {
+                                    data.map((option, o_index) => (
+                                        this.state.selected.find(s => s === option.key) == null ?
+                                            <Option key={`option-${index}-${o_index}`} value={option.key}>{option.value}</Option>
+                                        : null
+                                    ))
+                                }
+                                {
+                                    <Option value={"delete"}>{deleteText}</Option>
+                                }
+                            </Select>
+                        </div>
+                    ))
+                }
+                {
+                    this.state.selected.length < data.length ?
+                    <div>
+                        <Select labelInValue value={{key: this.state.newSelector}} style={{width: '100%'}} size={size}
+                                onChange={item => this.handleChange(item, "new")}>
+                            {
+                                data.map((option, o_index) => (
+                                    this.state.selected.find(s => s === option.key) == null ?
+                                        <Option key={`option-${o_index}`} value={option.key}>{option.value}</Option>
+                                        : null
+                                ))
+                            }
+                        </Select>
+                    </div>
+                    : null
+                }
+            </div>
+        );
+    }
+}
+
+export default MultipleSelector;
